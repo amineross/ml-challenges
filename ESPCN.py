6f17a0783f2135bb46f6a50d5220df99eac97428
Amine ROSTANE <72439293+amineross@users.noreply.github.com>
2025-09-02 16:38:27 +0200

    Refactor ESPCN class to accept scale as a parameter and add PSNR calculation method

--- ESPCN.py
+++ ESPCN.py
@@ -1,10 +1,12 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 
 class ESPCN(nn.Module):
-    def __init__(self, inputSize, outputSize, scale):
+    def __init__(self, scale=4):
         super().__init__()
+        self.scale = scale
         self.couches = nn.Sequential(
             nn.Conv2d(3, 64, 5, padding=2),
             nn.Tanh(),
@@ -17,3 +19,13 @@ class ESPCN(nn.Module):
     
     def forward(self, x):
         return self.couches(x)
+    
+    def pnsr(self, y, y_hat, n, m, d: float = 255.0):
+        if isinstance(y, torch.Tensor):
+            y = y.detach().cpu().numpy()
+        if isinstance(y_hat, torch.Tensor):
+            y_hat = y_hat.detach().cpu().numpy()
+        sse = ((y - y_hat) ** 2).sum()
+        if sse == 0.0:
+            return float('inf')
+        return 10.0 * np.log10((n * m * 3 * (d ** 2)) / sse)
